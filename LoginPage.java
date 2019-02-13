14be9d28434723200051c05b4b69874bbc3e4d4c
dhiyanesh92 <dhiyanesh2992@gmail.com>
2019-02-13 18:48:52 +1300

    Add files via upload

---
+++ LoginPage.java
@@ -0,0 +1,48 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+	
+	public WebDriver driver;
+	
+	By email=By.id("email");
+	By password=By.id("pass");
+	By login =By.id("loginbutton");
+	
+	
+	
+	
+
+
+
+	public LoginPage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver = driver ;
+	}
+
+
+
+
+
+	public WebElement getEmail()
+	{
+	return	driver.findElement(email);
+	
+	}
+	
+	public WebElement getPassword()
+	{
+	return	driver.findElement(password);
+	
+	}
+	
+	public WebElement getLogin()
+	{
+	return	driver.findElement(login);
+	
+	}
+
+}
